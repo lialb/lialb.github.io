e5b86a7ffab61fc06be6f49d1f741dc1b31c9e20
Albert <albert.yiliang@gmail.com>
2023-04-01 13:32:56 -0700

    Add Icon Tray

---
+++ sushi/src/components/shared/IconTray.tsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames';
+
+import 'src/components/shared/Icons.css';
+
+type Props = {
+  iconSize:
+    | 'xs'
+    | 'lg'
+    | 'sm'
+    | '1x'
+    | '2x'
+    | '3x'
+    | '4x'
+    | '5x'
+    | '6x'
+    | '7x'
+    | '8x'
+    | '9x'
+    | '10x';
+  darkMode: boolean;
+};
+
+export const IconTray = ({ iconSize, darkMode }: Props): React.ReactElement => {
+  const iconClassName = classNames(
+    { 'social-icon-dark': !darkMode },
+    { 'social-icon-light': darkMode }
+  );
+
+  return (
+    <div className="social-icons">
+      <br />
+      <a href="mailto:albert.yiliang@gmail.com">
+        <FontAwesomeIcon
+          icon="envelope"
+          className={iconClassName}
+          size={iconSize}
+        />
+      </a>
+      <a
+        href="https://github.com/albearli"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FontAwesomeIcon
+          icon={['fab', 'github']}
+          id="github"
+          className={iconClassName}
+          size={iconSize}
+        />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/albs/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FontAwesomeIcon
+          icon={['fab', 'linkedin']}
+          id="linkedin"
+          className={iconClassName}
+          size={iconSize}
+        />
+      </a>
+      <a
+        href="./Albert-Li-Resume.pdf"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FontAwesomeIcon
+          icon="file-alt"
+          className={iconClassName}
+          size={iconSize}
+        />
+      </a>
+    </div>
+  );
+};
